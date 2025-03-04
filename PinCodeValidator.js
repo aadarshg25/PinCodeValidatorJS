1b83fed0a7024f60c228aacf966bd12b6ced4cd5
aadarsh <aadarshg2562003@example.com>
2025-03-04 16:04:57 +0530

    [Aadarsh] Add . UC-4 feature allow space between pin code

--- PinCodeValidator.js
+++ PinCodeValidator.js
@@ -1,12 +1,12 @@
 //regex to validate pincode
-const regex = /^[0-9]{6}$/;
+const regex = /^[0-9]{3}\s?[0-9]{3}$/;
 
 //function to validate pin code
-function validate(pincode){
-    return regex.test(pincode);
+function validate(pincode) {
+    return regex.test(String(pincode)); 
 }
 
 console.log(validate(400088));
 console.log(validate("A40088"));
 console.log(validate("400088B"));
-
+console.log(validate("400 088"));
